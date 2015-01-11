166d2ff87bca0d6f70bf0abf796fd3f0ccd1eb86
Faun <woecifaun@gmail.com>
2015-01-11 22:28:15 +0100

    Init first working version (no style, few checks)

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+var environments = document.querySelectorAll('.environment');
+
+function updateBookmarklets(){
+    var chunks = [];
+    [].forEach.call(environments, function(environment) {
+        chunks.push(environment.querySelector('.url-chunk').value.replace('/', '\\/'));
+    });
+console.log(chunks);
+    [].forEach.call(environments, function(environment) {
+        updateBookmarklet(environment, chunks);
+    });
+
+}
+
+function updateBookmarklet(environment, chunks){
+    var bookmarklet = environment.querySelector('.bookmarklet');
+    bookmarklet.href = 
+        "javascript:"
+        +"(function(){location.assign(document.URL.replace(/"
+        +chunks.join("|")
+        +"/,'"
+        +environment.querySelector('.url-chunk').value
+        +"'))})();"
+    ;
+    bookmarklet.innerHTML = environment.querySelector('.button-name').value;
+}
+
+(function(){
+    updateBookmarklets();
+
+    var inputs = document.querySelectorAll('.url-chunk, .button-name');
+
+    [].forEach.call(inputs, function(input) {
+        input.onchange = updateBookmarklets;
+    });
+})();
